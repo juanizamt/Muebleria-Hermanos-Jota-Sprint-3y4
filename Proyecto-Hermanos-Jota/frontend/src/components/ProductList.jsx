0af5f42d3669652658c959ccd9863f31128ef8da
felipemartinez-code <fnmartinez@frd.utn.edu.ar>
2025-10-05 21:03:54 -0300

    Update ProductList.jsx

--- Proyecto-Hermanos-Jota/frontend/src/components/ProductList.jsx
+++ Proyecto-Hermanos-Jota/frontend/src/components/ProductList.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 function ProductList({ products, onSelectProduct }) {
+  if (!Array.isArray(products)) {
+    return null;
+  }
   return (
     <div className="products-container">
       {products.map(product => (
